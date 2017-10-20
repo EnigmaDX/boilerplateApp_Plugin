077646291fd67e8e088980f936faff73c3f6766f
EnigmaDX <derryckdxd@gmail.com>
2017-10-20 14:29:59 +0100

    added button to hide location

--- js/app/app.js
+++ js/app/app.js
@@ -15,95 +15,66 @@
     //     navigator.geolocation.getCurrentPosition(onSuccess, onError);
     // }
     $(document).ready(function(){
-      // initMap();
-      $('#btnPosition').on('click', function()
-      {
-        console.log("Button CLicked");
-        document.getElementById('map').style.visibility = "visible";
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-      });
       
-    });
-    
-     var map;
-    // onSuccess Geolocation
-    //
-    function onSuccess(position) {
-      
-      var currentLocation = {lat:  position.coords.latitude, lng: position.coords.longitude};
-      var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
-        center: currentLocation
-      });
-      
-      var marker = new google.maps.Marker({
-        position: currentLocation,
-        map: map
-      });
-      
-      
-      var element = document.getElementById('geolocation');
-      element.innerHTML = 'Latitude: ' + position.coords.latitude  + '<br />' +
-      'Longitude: '          + position.coords.longitude             + '<br />' +
-      'Altitude: '           + position.coords.altitude              + '<br />' +
-      'Accuracy: '           + position.coords.accuracy              + '<br />' +
-      'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-      'Heading: '            + position.coords.heading               + '<br />' +
-      'Speed: '              + position.coords.speed                 + '<br />' +
-      'Timestamp: '          + position.timestamp          + '<br />';
-    }
-    
-    // onError Callback receives a PositionError object
-    //
-    function onError(error) {
-      alert('code: '    + error.code    + '\n' +
-      'message: ' + error.message + '\n');
-    }
-    
-   
-    function initMap() {
-      
-      
-    }
-    
-    // var map, infoWindow;
-    //       function initMap() {
-      //         map = new google.maps.Map(document.getElementById('gMap'), {
-        //           center: {lat: -34.397, lng: 150.644},
-        //           zoom: 6
-        //         });
-        //         infoWindow = new google.maps.InfoWindow;
+      var currentLocation;
+      var map;
+      var marker;
+      function onSuccess(position) {
         
-        //         // Try HTML5 geolocation.
-        //         if (navigator.geolocation) {
-          //           navigator.geolocation.getCurrentPosition(function(position) {
-            //             var pos = {
-              //               lat: position.coords.latitude,
-              //               lng: position.coords.longitude
-              //             };
-              
-              //             infoWindow.setPosition(pos);
-              //             infoWindow.setContent('Location found.');
-              //             infoWindow.open(map);
-              //             map.setCenter(pos);
-              //           }, function() {
-                //             handleLocationError(true, infoWindow, map.getCenter());
-                //           });
-                //         } else {
-                  //           // Browser doesn't support Geolocation
-                  //           handleLocationError(false, infoWindow, map.getCenter());
-                  //         }
-                  //       }
-                  
-                  //       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-                    //         infoWindow.setPosition(pos);
-                    //         infoWindow.setContent(browserHasGeolocation ?
-                    //                               'Error: The Geolocation service failed.' :
-                    //                               'Error: Your browser doesn\'t support geolocation.');
-                    //         infoWindow.open(map);
-                    //       }
-                    
-                    
-                    
-                    
-                    
+        currentLocation = {lat:  position.coords.latitude, lng: position.coords.longitude};
+          map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 8,
+          center: currentLocation
+        });
+          
+          // onSuccess Geolocation
+          // var element = document.getElementById('geolocation');
+          // element.innerHTML = 'Latitude: ' + position.coords.latitude  + '<br />' +
+          // 'Longitude: '          + position.coords.longitude             + '<br />' +
+          // 'Altitude: '           + position.coords.altitude              + '<br />' +
+          // 'Accuracy: '           + position.coords.accuracy              + '<br />' +
+          // 'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+          // 'Heading: '            + position.coords.heading               + '<br />' +
+          // 'Speed: '              + position.coords.speed                 + '<br />' +
+          // 'Timestamp: '          + position.timestamp          + '<br />';
+        }
+        
+        // onError Callback receives a PositionError object
+        //
+        function onError(error) {
+          alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+        }
+        
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        
+        $('#btnShowPos').on('click', function()
+        {
+            marker = new google.maps.Marker({
+            position: currentLocation,
+            map: map
+          });
+
+          map.setZoom(12);
+          map.setCenter(marker.getPosition());
+        });
+
+        $('#btnHidePos').on('click', function()
+            {
+                marker.setMap(null);
+                map.setZoom(8);
+            });
+        
+      });
+      
+      
+      
+      
+      
+      
+      function initMap() {    }
+                      
+                      
+                      
+                      
+                      
